b5e984254d6179cc28d46dd238f57005281411db
Sven Jost <sven@wire.com>
2017-04-21 14:04:46 +0200

    Add some formating to help info

--- src/main/java/com/wire/bots/github/MessageHandler.java
+++ src/main/java/com/wire/bots/github/MessageHandler.java
@@ -70,10 +70,10 @@ public class MessageHandler extends MessageHandlerBase {
     private String getHelp(String host, String secret, String botId) {
         return String.format("Hi, I'm GitHub-Bot. Here is how to set me up:\n\n"
                         + "1. Go to the repository that you want to connect to\n"
-                        + "2. Go to Settings / Webhooks / Add webhook\n"
-                        + "3. Add Payload URL: https://%s/github/%s\n"
-                        + "4. Set Content-Type: application/json\n"
-                        + "5. Set Secret: %s",
+                        + "2. Go to **Settings / Webhooks / Add webhook**\n"
+                        + "3. Add **Payload URL**: https://%s/github/%s\n"
+                        + "4. Set **Content-Type**: application/json\n"
+                        + "5. Set **Secret**: %s",
                 host,
                 botId,
                 secret);
